34bc462438977189a14ca11b2f34f476548678e3
Mitch Stonehocker <45083594+MitchStonehocker@users.noreply.github.com>
2019-07-29 14:37:17 -0400

    Update index.js

--- src/utils/index.js
+++ src/utils/index.js
@@ -1,6 +1,6 @@
 // src/utils/index.js
 
-const wcBaseUrl = "https://www.wolframcloud.com/objects/mitch/";
+const wcBaseUrl = "... get your Wolfram CloudDase by evaluating $CloudBase in a notebook ...";
 const wcEndPoint = "live-coding/";
 
 export { wcBaseUrl, wcEndPoint };
